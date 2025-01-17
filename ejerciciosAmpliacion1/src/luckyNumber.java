b587b1559dcc2f4f3cd69a0ca3e28ec677f46d48
PabloPcGaming <pabmoranc@alu.edu.gva.es>
2025-01-17 13:31:44 +0100

    Acabado luckyNumber

--- ejerciciosAmpliacion1/src/luckyNumber.java
+++ ejerciciosAmpliacion1/src/luckyNumber.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class luckyNumber {
+    public static void main(String[] args) {
+
+        String fechaNacimiento = "18-04-2006";
+
+        int luckyNumber = calcuclarNumeroLucky(fechaNacimiento);
+
+        System.out.println(luckyNumber);
+    }
+
+    public  static int calcuclarNumeroLucky(String fechaNacimiento){
+        List<String> lista = new ArrayList<>(List.of(fechaNacimiento.split("")));
+
+        int sum =10;
+
+        while(sum > 9) {
+
+            sum = lista.stream()
+                    .filter(s -> !s.equals("-"))
+                    .mapToInt(s -> Integer.valueOf(s))
+                    .reduce(0, (previousSum, value) -> previousSum + value);
+
+            lista = new ArrayList<>(List.of(String.valueOf(sum).split("")));
+        }
+
+        return sum;
+
+    }
 }
